9ba7404ad664cc31d37ce61bdc13edceab16e9b9
Andrew <stewa2a8@mail.uc.edu>
2022-09-18 23:20:57 -0400

    completed monthly credit card intrest values

--- src/MonthlyCCBalance.java
+++ src/MonthlyCCBalance.java
@@ -6,7 +6,7 @@ public class MonthlyCCBalance {
             double balance = 5000;
             double INTREST_RATE = 0.17;
             double firstMonthsIntrest = balance * INTREST_RATE;
-            double secondMonthsIntrest = firstMonthsIntrest * INTREST_RATE;
+            double secondMonthsIntrest = (balance + firstMonthsIntrest) * INTREST_RATE;
             System.out.println("Your first months intrest payment is $ " + firstMonthsIntrest);
             System.out.println("Your second months intrest payment is $ " + secondMonthsIntrest);
 
